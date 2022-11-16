c5298583a78dae49406ad5b132ede3ed087004e2
edcarlos <efdsilva84@gmail.com>
2022-11-16 15:29:43 -0400

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # nodests-canil
+
+# pré requisitos globais
+npm install -g nodemon typescript ts-node
+#rodar o projeto
+    "start-dev": "nodemon -e ts,json,mustache src/server.ts"
+# instalcao
+npm install
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,27 @@
+import { Request , Response } from 'express';
+
+export const home = (req: Request, res: Response) =>{
+        res.send("tela de home")
+    //res.render('pages/search');
+
+}
+export const dogs = (req: Request, res: Response) =>{
+
+    //res.render('pages/search');
+
+}
+export const cats = (req: Request, res: Response) =>{
+
+    //res.render('pages/search');
+
+}
+export const fishes = (req: Request, res: Response) =>{
+
+    //res.render('pages/search');
+
+}
+export const search = (req: Request, res: Response) =>{
+
+    //res.render('pages/search');
+
+}
